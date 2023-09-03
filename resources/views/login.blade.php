e4f64548cf5ff9267b89d7ef5066c3f3c49c4073
ryhanauliaatreeee <inireyhann03@gmail.com>
2023-09-03 11:21:02 +0700

    menambah login form

---
+++ resources/views/login.blade.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300&display=swap" rel="stylesheet">
+    <link rel="icon" href="img/fire.png">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <style>
+
+      *{
+        font-family: 'Raleway', sans-serif;
+       }
+
+        body{
+            background-image: url(public/poto/rawr.png);
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-position: center;
+  }
+        
+        .cardlog{
+            padding-top: 2px;
+        }
+
+        .card{
+            padding-bottom: 35px;
+            padding-top:20px;
+            position: relative;
+        }
+
+        .jud{
+            font-size: 60px;
+        }
+
+    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  </head>
+  <body class="bg-image" style="background-image: url(img/bg.png);">
+
+    
+
+ 
+      
+
+      
+      
+    
+      
+  
+
+      <body>
+        <center>
+    <div class="row-md-5 " style="margin-top: 170px;">
+        <div class="cardlog col-md-3">
+            <div class="login card border-0 shadow rounded-4 my-1">
+                <div class="card-body p-5 p-sm-12">
+                    <img src="public/poto/logo.png" class="mx-auto d-block mb-4" alt="wikr" width="80px" style="border-radius: 100%;">
+                    <form action="" method="post">
+                        <div class="form-floating mb-5">
+                            <input type="text" name="username" class="form-control" id="username" placeholder="Username" autofocus required>
+                            <label for="usn">Username</label>
+                        </div>
+                        <div class="form-floating mb-5">
+                            <input type="password" name="pass" class="form-control" id="pass" placeholder="Password" required>
+                            <label for="pass">Password</label>
+                        </div>
+                        <div class="d-flex justify-content-center">
+                            <button type="submit" name="submit" class="btn btn-login text-uppercase fw-bold" style="background-color: #AEDBCE; color: white; border-radius: 100px; font-weight:bold; padding-left: 50px; padding-right: 50px;">Login</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    </center>
+</body>
+</html>
+      
+
+
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>   
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+  </body>
+</html>
