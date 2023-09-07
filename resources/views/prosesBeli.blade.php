92d8f1e2959157d4094ddde18ec11a25030fe66d
AbiyuRafi <abiyurafii09@gmail.com>
2023-09-07 16:46:17 +0700

    laravel

--- resources/views/prosesBeli.blade.php
+++ resources/views/prosesBeli.blade.php
@@ -1,17 +1,16 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <style>
-        .display{
-  background-image: url('{{asset('poto/rawr.png')}}');
-  background-size: cover;
-  height: 100vh;
-  
-  
-}
+        .display {
+            background-image: url('{{ asset('poto/rawr.png') }}');
+            background-size: cover;
+            height: 100vh;
+
+
+        }
+
         #open {
             color: #2A8271;
             cursor: pointer;
@@ -36,7 +35,7 @@
 
         .model-container {
             display: flex;
-          
+
             align-items: center;
             justify-content: center;
             min-width: 100vw;
@@ -59,35 +58,34 @@
             padding-bottom: 40px;
         }
 
-        .model-container button{
-            margin:20px;
-          
+        .model-container button {
+            margin: 20px;
+
         }
     </style>
     @extends('layout.mainlayout')
-    <link rel="stylesheet" href="{{asset('css/beli.css')}}">
-    @section('title','Chekout | yasmavoca')
+    <link rel="stylesheet" href="{{ asset('css/beli.css') }}">
+    @section('title', 'Chekout | yasmavoca')
 
     @section('content')
 
-    <div class="display">
+        <div class="display">
 
 
-        <div class="slider">
-            <div class="sliderWrapper">
-                <div class="sliderItem">
+            <div class="slider">
+                <div class="sliderWrapper">
+                    <div class="sliderItem">
 
-                    <img src="{{asset('poto/miki.png')}}" alt="" class="sliderImg">
-                    <div class="title" style="font-size: 24px; font-weight:700; color: black;">Alpukat miki</div>
-                    <div class="sliderprice">100.000/kg</div>
-                    
+                        <img src="{{ asset('poto/miki.png') }}" alt="" class="sliderImg">
+                        <div class="title" style="font-size: 24px; font-weight:700; color: black;">Alpukat miki</div>
+                        <div class="sliderprice">100.000/kg</div>
+
                         <p style="width: 500px;">Alpukat Miki merupakan varietas alpukat unggulan Indonesia. Varietas
                             Alpukat yang satu ini dikembangkan oleh Pusat Kajian Buah-Buahan Tropika. Buah alpukat miki
                             dikenal memiliki tampilan yang imut ini memang memiliki citarasa yang istimewa.</p>
-                        <i class='bx bxs-star' style="color:yellow"></i><i class='bx bxs-star'
-                            style="color:yellow"></i><i class='bx bxs-star' style="color:yellow"></i><i
-                            class='bx bxs-star' style="color:yellow"></i><i class='bx bxs-star-half'
-                            style="color:yellow"></i>
+                        <i class='bx bxs-star' style="color:yellow"></i><i class='bx bxs-star' style="color:yellow"></i><i
+                            class='bx bxs-star' style="color:yellow"></i><i class='bx bxs-star' style="color:yellow"></i><i
+                            class='bx bxs-star-half' style="color:yellow"></i>
                         <div class="container">
                             <button><a href="" id="open">Beli Disini</a></button>
                         </div>
@@ -99,34 +97,33 @@
                                     <input name="pesanan" type="number" placeholder="pesanan" required><br>
                                     <input name="alamat" type="text" placeholder="alamat" required><br>
                                     <input name="deskripsi" type="text" placeholder="deskripsi">
-                                    <p style="margin-bottom:30px">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, quod?</p>
+                                    <p style="margin-bottom:30px">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                        Eos, quod?</p>
                                     <button>Beli</button>
-                                    <button ><a href="/beli" style="color:#2A8271">cancel</a></button>
+                                    <button><a href="/beli" style="color:#2A8271">cancel</a></button>
                                 </form>
                             </div>
                         </div>
+
+                    </div>
+
 
                 </div>
 
 
             </div>
 
-
         </div>
 
-    </div>
 
 
 
-
-    <script src="beli.js"></script>
-    <script type="text/javascript">
-        $(document).ready(function () {
-            $('#open').click(function () {
-                $('.model-container').css('transform', 'scale(1)');
-            });
-        })
-    </script>
-</body>
-
-
+        <script src="beli.js"></script>
+        <script type="text/javascript">
+            $(document).ready(function() {
+                $('#open').click(function() {
+                    $('.model-container').css('transform', 'scale(1)');
+                });
+            })
+        </script>
+        </body>
